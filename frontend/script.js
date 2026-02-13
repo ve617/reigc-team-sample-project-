5f2090d2e02bce6a4c8486ecb498e1dc1c8e4438
rupakumari23 <rupakumari2939@gmail.com>
2026-02-13 11:38:26 +0530

    poll for create

--- frontend/script.js
+++ frontend/script.js
@@ -4,3 +4,19 @@ function likePost() {
     likeCount++;
     document.getElementById("likeCount").innerText = likeCount;
 }
+
+//poll
+
+let newsVotes = 0;
+let blogVotes = 0;
+
+function vote(type) {
+    if (type === "news") {
+        newsVotes++;
+        document.getElementById("newsVotes").innerText = newsVotes;
+    } else {
+        blogVotes++;
+        document.getElementById("blogVotes").innerText = blogVotes;
+    }
+}
+
